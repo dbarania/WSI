d07b25b03af2c5320ebcd196668502c59ee30769
dbarania <baraniak.damian@gmail.com>
2024-04-09 21:24:10 +0200

    AlfaBeta implemented

--- solutions/E3/minimax_tictactoe.py
+++ solutions/E3/minimax_tictactoe.py
@@ -1,18 +1,42 @@
 import numpy as np
 import copy
-from utils import check_board,generate_next_moves
+from utils import check_board, generate_next_moves
 
-def MiniMaxTicTacToe(board:np.ndarray,depth:int,move_max:bool):
-    pay = check_board(board,(1 if not move_max else -1))
+
+def MiniMaxTicTacToe(board: np.ndarray, depth: int, move_max: bool):
+    pay = check_board(board, (1 if not move_max else -1))
     if pay or not depth:
-        return pay*(depth+1)
-    best  = -1000 if move_max else 1000
+        return pay * (depth + 1)
+    best = -1000 if move_max else 1000
     moves = generate_next_moves(board)
     for move in moves:
         new_board = copy.copy(board)
         new_board[*move] = 1 if move_max else -1
         if move_max:
-            best = max(best,MiniMaxTicTacToe(new_board,depth-1,not move_max))
-        else:  
-            best = min(best,MiniMaxTicTacToe(new_board,depth-1,not move_max))
+            best = max(best, MiniMaxTicTacToe(new_board, depth - 1, not move_max))
+        else:
+            best = min(best, MiniMaxTicTacToe(new_board, depth - 1, not move_max))
     return best
+
+
+def AlfaBeta(board: np.ndarray, depth: int, move_max: bool, alfa=-1e6, beta=1e6):
+    pay = check_board(board, (1 if not move_max else -1))
+    if pay or not depth:
+        return pay * (depth + 1)
+    moves = generate_next_moves(board)
+    if move_max:
+        for move in moves:
+            new_board = copy.copy(board)
+            new_board[*move] = 1 if move_max else -1
+            alfa = max(alfa, AlfaBeta(new_board, depth - 1, not move_max, alfa, beta))
+            if alfa >= beta:
+                return alfa
+        return alfa
+    else:
+        for move in moves:
+            new_board = copy.copy(board)
+            new_board[*move] = 1 if move_max else -1
+            beta = min(beta, AlfaBeta(new_board, depth - 1, not move_max, alfa, beta))
+            if alfa >= beta:
+                return beta
+        return beta
--- solutions/E3/tictactoe.py
+++ solutions/E3/tictactoe.py
@@ -1,50 +1,54 @@
 import numpy as np
 import random
-from minimax_tictactoe import MiniMaxTicTacToe
+from minimax_tictactoe import MiniMaxTicTacToe, AlfaBeta
 from utils import check_board, generate_next_moves
 import copy
-temp_board = np.array([[-1,1,-1],[-1,1,1],[0,0,-1]])
-class Tictactoe:
+
+temp_board = np.array([[-1, 1, -1], [-1, 1, 1], [0, 0, -1]])
+
+
+class TicTacToe:
     PLAYER = -1
     COMPUTER = 1
-    def __init__(self,filepath) -> None:
-        self.board = np.zeros((3,3))
+
+    def __init__(self, filepath) -> None:
+        self.board = np.zeros((3, 3))
         self.move_counter = 0
-        self.file = open(filepath,"+a")
+        self.file = open(filepath, "+a")
         self.who_is_moving = None
 
-
-    def str_cell(self,value):
+    @staticmethod
+    def str_cell(value):
         if np.sign(value) == 1:
             return "X"
-        elif np.sign(value) ==-1:
+        elif np.sign(value) == -1:
             return "O"
-        else: 
+        else:
             return " "
 
     def __str__(self) -> str:
         r = ""
         for row in self.board:
-            s = f"{self.str_cell(row[0])}|{self.str_cell(row[1])}|{self.str_cell(row[2])}\n{'-'*5}\n"
-            r+=s
+            s = f"{self.str_cell(row[0])}|{self.str_cell(row[1])}|{self.str_cell(row[2])}\n{'-' * 5}\n"
+            r += s
         return r.rstrip("----\n")
-    
+
     def clear_data(self):
-        self.board = np.zeros((3,3))
+        self.board = np.zeros((3, 3))
         self.move_counter = 0
         self.who_is_moving = None
 
     def player_move(self):
         while True:
-            row = int(input("Choose a row: "))-1
-            column = int(input("Coose column: "))-1
+            row = int(input("Choose a row: ")) - 1
+            column = int(input("Choose column: ")) - 1
             if not self.board[row][column]:
                 self.board[row][column] = self.who_is_moving
                 return
             else:
                 print("Invalid input try again")
                 continue
-                
+
     def computer_move(self):
         moves = (generate_next_moves(self.board))
         random.shuffle(moves)
@@ -55,24 +59,24 @@ class Tictactoe:
             for move in moves:
                 new_board = copy.copy(self.board)
                 new_board[*move] = 1
-                result = MiniMaxTicTacToe(new_board,8,False)
-                if result> score:
+                result = AlfaBeta(new_board, 8, False)
+                if result > score:
                     best_move = move
                     score = result
                 d[move] = result
             self.board[*best_move] = self.who_is_moving
 
     def update_move(self):
-        if self.who_is_moving== self.COMPUTER:
+        if self.who_is_moving == self.COMPUTER:
             self.who_is_moving = self.PLAYER
         else:
             self.who_is_moving = self.COMPUTER
-        
+
     def game(self):
         if not self.who_is_moving:
-            # self.who_is_moving = random.choice([self.PLAYER,self.COMPUTER])
-            #temp
-            self.who_is_moving = self.COMPUTER
+            self.who_is_moving = random.choice([self.PLAYER, self.COMPUTER])
+            # temp
+            # self.who_is_moving = self.COMPUTER
         state_of_game = True
         while state_of_game:
             self.move_counter += 1
@@ -83,12 +87,11 @@ class Tictactoe:
                     self.player_move()
                 case self.COMPUTER:
                     self.computer_move()
-            state_of_game = not check_board(self.board,self.who_is_moving)
+            state_of_game = not check_board(self.board, self.who_is_moving)
             self.update_move()
 
 
-
-if __name__=="__main__":#
-    g = Tictactoe("t.log")
-    g.board = temp_board
+if __name__ == "__main__":  #
+    g = TicTacToe("t.log")
+    # g.board = temp_board
     g.game()
